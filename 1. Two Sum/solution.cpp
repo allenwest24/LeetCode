3548f6acc33326213f9b12e706ca1396ed4e60f5
Allen West <58156836+allenwest24@users.noreply.github.com>
2020-06-03 13:43:22 -0400

    Update solution.cpp

--- 1. Two Sum/solution.cpp
+++ 1. Two Sum/solution.cpp
@@ -2,9 +2,12 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> a{0, 0};
-        for (int ii = 0; ii < nums.size(); ++ii) {
-            for (int jj = ii + 1; jj < nums.size(); ++jj) {
-                if (nums[ii] + nums[jj] == target) {
+        int curr = 0;
+        int size = nums.size();
+        for (int ii = 0; ii < size; ++ii) {
+            curr = nums[ii];
+            for (int jj = ii + 1; jj < size; ++jj) {
+                if (curr + nums[jj] == target) {
                     a[0] = ii;
                     a[1] = jj;
                     return a;
