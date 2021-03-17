dcf46a30c120748884867f9feaef6e8e88e82141
Allen West <58156836+allenwest24@users.noreply.github.com>
2021-03-17 18:31:03 -0400

    Create solution.rs

---
+++ 709. To Lower Case/solution.rs
@@ -0,0 +1,8 @@
+impl Solution {
+    pub fn to_lower_case(str: String) -> String {
+        // Sorta cheating but whatever it works.
+        // Lazy solution = better solution.
+        let lower = str.to_lowercase();
+        return lower;
+    }
+}
