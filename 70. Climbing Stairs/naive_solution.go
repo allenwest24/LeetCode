f8908131f21e714f3dbfc1ac537907b7940aae66
Allen West <58156836+allenwest24@users.noreply.github.com>
2022-11-01 18:50:10 -0400

    Create naive_solution.go

---
+++ 70. Climbing Stairs/naive_solution.go
@@ -0,0 +1,9 @@
+func climbStairs(n int) int {
+    if n == 1 {
+        return 1
+    } else if n == 2 {
+        return 2
+    } else {
+        return climbStairs(n - 1) + climbStairs(n - 2)
+    }
+}
